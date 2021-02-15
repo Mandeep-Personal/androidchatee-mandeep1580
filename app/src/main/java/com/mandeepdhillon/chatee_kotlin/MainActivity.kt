d8bd96c5b590848c48df6924eb025e078452b1a1
mandeep1580 <mandeep15@gmail.com>
2021-02-14 23:41:55 -0800

    Added new activity for login along with the layout, also added the relevant contstraints

--- app/src/main/java/com/mandeepdhillon/chatee_kotlin/MainActivity.kt
+++ app/src/main/java/com/mandeepdhillon/chatee_kotlin/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.mandeepdhillon.chatee_kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mandeepdhillon.chatee_kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.loginButton.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
     }
 }
