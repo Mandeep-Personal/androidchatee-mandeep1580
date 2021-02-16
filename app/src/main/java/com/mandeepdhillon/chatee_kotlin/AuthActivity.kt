8458c9ee4508689d141b5828e074b30790d83724
mandeep1580 <mandeep15@gmail.com>
2021-02-15 19:39:24 -0800

    Integrated firebase and created the authentication functions in the AuthActivity for both login and register.

--- app/src/main/java/com/mandeepdhillon/chatee_kotlin/AuthActivity.kt
+++ app/src/main/java/com/mandeepdhillon/chatee_kotlin/AuthActivity.kt
@@ -6,10 +6,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.mandeepdhillon.chatee_kotlin.databinding.ActivityAuthBinding
 
-class AuthActivity: AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityAuthBinding
     private lateinit var authMode: AuthMode
 
@@ -17,7 +19,7 @@ class AuthActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        auth = FirebaseAuth.getInstance()
         authMode = intent?.extras?.getParcelable(AuthModeExtraName)!!
 
         when (authMode) {
@@ -28,17 +30,45 @@ class AuthActivity: AppCompatActivity() {
                 binding.authButton.text = getString(R.string.register)
             }
         }
+        binding.authButton.setOnClickListener {
+            when (authMode) {
+                is AuthMode.Login -> {
+                    auth.signInWithEmailAndPassword(binding.emailInput.text.toString(), binding.passwordInput.text.toString())
+                            .addOnCompleteListener(this) { task ->
+                                if (task.isSuccessful) {
+                                    val user = auth.currentUser
+                                    startActivity(Intent(this, ChatActivity::class.java))
+                                }
+                                // Handle error here
+                            }
+                }
+
+                is AuthMode.Register -> {
+                    auth.createUserWithEmailAndPassword(binding.emailInput.text.toString(), binding.passwordInput.text.toString())
+                            .addOnCompleteListener(this) { task ->
+                                if (task.isSuccessful) {
+                                    val user = auth.currentUser
+                                    startActivity(Intent(this, ChatActivity::class.java))
+                                }
+                                // Handle error here
+                            }
+                }
+
+            }
+        }
+
 //    fun Activity.hideKeyboard(){
 //        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 //        imm.hideSoftInputFromWindow(this.currentFocus?.windowToken, 0);
 //    }
     }
-        companion object {
-            const val AuthModeExtraName = "AUTH_MODE"
-            fun newIntent(authMode: AuthMode, context: Context): Intent {
-                val intent = Intent(context, AuthActivity::class.java)
-                intent.putExtra(AuthModeExtraName, authMode)
-                return intent
-            }
+
+    companion object {
+        const val AuthModeExtraName = "AUTH_MODE"
+        fun newIntent(authMode: AuthMode, context: Context): Intent {
+            val intent = Intent(context, AuthActivity::class.java)
+            intent.putExtra(AuthModeExtraName, authMode)
+            return intent
         }
     }
+}
