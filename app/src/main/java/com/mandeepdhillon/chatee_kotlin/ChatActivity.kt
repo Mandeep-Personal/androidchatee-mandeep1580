6d784fa130bce9cbf9a7c47ead110c16ff394414
mandeep1580 <mandeep15@gmail.com>
2021-02-15 23:31:32 -0800

    chat activity configured with Firebase API and sent data to realtime database.

--- app/src/main/java/com/mandeepdhillon/chatee_kotlin/ChatActivity.kt
+++ app/src/main/java/com/mandeepdhillon/chatee_kotlin/ChatActivity.kt
@@ -3,15 +3,35 @@ package com.mandeepdhillon.chatee_kotlin
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.mandeepdhillon.chatee_kotlin.databinding.ActivityChatBinding
 
 class ChatActivity: AppCompatActivity() {
     private lateinit var binding: ActivityChatBinding
+    private lateinit var messagesDB: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChatBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.textLabel.text = FirebaseAuth.getInstance().currentUser?.email
+        messagesDB = FirebaseDatabase.getInstance().getReference("Messages")
+
+        // set an onClickListener on the send button to send the user email and message to a save function
+        binding.sendMessageButton.setOnClickListener {
+            val sender = FirebaseAuth.getInstance().currentUser?.email
+            val message = binding.messageInput.text.toString()
+            if (sender != null) {
+                saveMessage(sender, message)
+            }
+        }
+    }
+    private fun saveMessage(sender: String, messageBody: String) {
+        val key = messagesDB.push().key
+        key ?: return
+        val message = Message(sender, messageBody)
+        messagesDB.child(key).setValue(message)
     }
 }
+
+data class Message(var sender: String = "",var messageBody: String = "")
